b242dca6ebbdb61d94471424bb1bd3e54bc261d1
Max Erickson <maxerickson@gmail.com>
2016-03-10 16:47:10 -0500

    Add project files.

---
+++ setup.py
@@ -0,0 +1,42 @@
+#   Copyright 2016 Max Erickson
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+import os
+from setuptools import find_packages, setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='greetings',
+    version='0.1',
+    packages=find_packages(),
+    include_package_data=True,
+    license='Apache License 2.0',
+    description='A simple Django app to send email greetings to Dr Chrono patients.',
+    long_description=README,
+    author='Max Erickson',
+    author_email='maxerickson@gmail.com',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 1.9',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+)
